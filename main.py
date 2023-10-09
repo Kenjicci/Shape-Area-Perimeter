9cb2ad7153f9ffce899b443f8023fb18a19bcb13
Kenjicci <Cincokencstd1@gmail.com>
2023-10-09 09:09:29 +0800

    main.py
    the format for circle and rectangle class

---
+++ main.py
@@ -0,0 +1,12 @@
+class Shape:
+    def __init__(self):
+        self.unit = "meters"
+
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+    def display(self):
+        pass
