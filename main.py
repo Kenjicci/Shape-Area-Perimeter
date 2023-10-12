31f4592a341d140eab3a8420113b55acdbe3bd4f
Tannerssss <balcetristan331@gmail.com>
2023-10-12 10:08:59 +0800

    ito na

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 class Shape:
     def __init__(self):
         self.unit = "meters"
@@ -10,3 +12,22 @@ class Shape:
 
     def display(self):
         pass
+class circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+    
+
+class rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * (self.width + self.height)
