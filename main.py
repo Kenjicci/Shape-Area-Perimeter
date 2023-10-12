8a80c35d590e219afd40676dc1d36af98121aecd
Kenjicci <Cincokencstd1@gmail.com>
2023-10-12 17:09:41 +0800

    Added display and round off

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import math
 
 class Shape:
-    def __init__(self):
-        self.unit = "meters"
+    def __init__(self, unit = "meters"):
+        self.unit = unit
 
     def area(self):
         pass
@@ -13,39 +13,43 @@ class Shape:
     def display(self):
         pass
 
-class Circle(Shape):
-    def __init__(self, r):
+class circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
         super().__init__()
-        self.r = r
 
     def area(self):
-        area = math.pi * (self.r ** 2)
-        return area
-
+        area = math.pi * self.radius ** 2
+        return round(area, 2)
+    
     def perimeter(self):
-        p = 2 * math.pi * self.r
-        return p
+        circumference = 2 * math.pi * self.radius
+        return round(circumference, 2)
 
     def display(self):
-        print(f"Circle  -  Radius: {self.r} {self.unit}")
+        print(f"Circle  -  Radius: {self.radius} {self.unit}")
         print(f"           Area: {self.area()} {self.unit} squared ")
-        print(f"           Perimeter: {self.perimeter()} {self.unit}")
+        print(f"           Perimeter: {self.perimeter()} {self.unit}")   
 
-class Rectangles(Shape):
-    def __init__(self, length, width):
-        super().__init__()
-        self.length = length
+class rectangle(Shape):
+    def __init__(self, width, height):
         self.width = width
+        self.height = height
+        super().__init__()
 
     def area(self):
-        area = self.width * self.length
-        return area
+        return self.width * self.height
     
     def perimeter(self):
-        perimeter = 2*(self.width) + 2*(self.length)
-        return perimeter
-    
+        return 2 * (self.width + self.height)
+
     def display(self):
-        print(f"Rectangle - Width: {self.width} {self.unit}, Height: {self.length} {self.unit}")
+        print(f"Rectangle - Width: {self.width} {self.unit}, Height: {self.height} {self.unit}")
         print(f"            Area: {self.area()} {self.unit} squared")
         print(f"            Perimeter: {self.perimeter()} {self.unit}")
+
+Circle = circle(5)
+Rectangle = rectangle(3,4)
+
+Circle.display()
+Rectangle.display()
